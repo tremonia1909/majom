4cbd8b5472917fab9ce8ec7dfe895e34f6f8dc7d
maltela <internet1000@freenet.de>
2015-01-11 17:48:02 +0100

    High_Voltage Fix #2

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 HighVoltage.configure do |config|
     config.route_drawer = HighVoltage::RouteDrawers::Root
 end
-=======
->>>>>>> 72108988ffac9e4d570435ebcde855a9d73443e2
+#=======
+#>>>>>>> 72108988ffac9e4d570435ebcde855a9d73443e2
