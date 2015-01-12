50de84125ee5063e645a7902391c774111a28f85
lag3o <lag_dab@hotmail.de>
2015-01-12 16:36:56 +0100

    Landingpage updated

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,6 +1,6 @@
-#<<<<<<< HEAD
 HighVoltage.configure do |config|
     config.route_drawer = HighVoltage::RouteDrawers::Root
 end
-#=======
-#>>>>>>> 72108988ffac9e4d570435ebcde855a9d73443e2
+HighVoltage.configure do |config|
+    config.home_page = 'landing'
+end
