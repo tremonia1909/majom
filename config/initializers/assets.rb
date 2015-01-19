1218d06eedd5820d22c0951fd498883de42c81af
lag3o <lag_dab@hotmail.de>
2015-01-19 20:13:06 +0100

    added Devise views, tried to make css work for devise

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( twitter/bootstrap/glyphicons-halflings.png )
 Rails.application.config.assets.precompile += %w( twitter/bootstrap/glyphicons-halflings-white.png )
+Rails.application.config.assets.precompile += %w( jquery-2.1.1.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,13 +1,6 @@
 HighVoltage.configure do |config|
-<<<<<<< HEAD
     config.route_drawer = HighVoltage::RouteDrawers::Root
 end
 HighVoltage.configure do |config|
     config.home_page = 'landing'
-=======
-  config.home_page = 'index'
-end
-HighVoltage.configure do |config|
-  config.route_drawer = HighVoltage::RouteDrawers::Root
->>>>>>> develop
 end
