34e5deedc0c8d7694bc50e5f8d2289516b75cd1a
lag3o <lag_dab@hotmail.de>
2015-01-22 11:34:44 +0100

    Error fixing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,16 +4,27 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( twitter/bootstrap/glyphicons-halflings.png )
 Rails.application.config.assets.precompile += %w( twitter/bootstrap/glyphicons-halflings-white.png )
+
+
 Rails.application.config.assets.precompile += %w( jquery-2.1.1.js )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( plugins/metisMenu/jquery.metisMenu.js )
+Rails.application.config.assets.precompile += %w( plugins/slimscroll/jquery.slimscroll.min.js )
+Rails.application.config.assets.precompile += %w( plugins/peity/jquery.peity.min.js )
+Rails.application.config.assets.precompile += %w( inspinia.js)
+Rails.application.config.assets.precompile += %w( plugins/pace/pace.min.js )
+Rails.application.config.assets.precompile += %w( plugins/jquery-ui/jquery-ui.min.js)
+Rails.application.config.assets.precompile += %w( plugins/iCheck/icheck.min.js )
+Rails.application.config.assets.precompile += %w( demo/peity-demo.js )
+
 Rails.application.config.assets.precompile += %w( animate.css )
 Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( landing/bootstrap.css )
 Rails.application.config.assets.precompile += %w( landing/style.css )
 Rails.application.config.assets.precompile += %w( landing/animate.min.css )
 Rails.application.config.assets.precompile += %w( landing/font-awesome/css/font-awesome.min.css )
-
+Rails.application.config.assets.precompile += %w( font-awesome/css/font-awesome.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
