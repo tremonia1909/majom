d765e961c81224e941a04d17a6d23aeed02cb9a2
maltela <internet1000@freenet.de>
2015-01-24 17:14:08 +0100

    Devise - Verlinken auf Index -

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+
+  def after_sign_out_path_for(resource_or_scope)
+    pages_index_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    pages_index_path
+  end
+
 end
