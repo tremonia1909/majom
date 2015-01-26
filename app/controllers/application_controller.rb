d64ba5a87405569a13ed11d99feacf4ec9e11f2d
lag3o <lag_dab@hotmail.de>
2015-01-26 15:50:58 +0100

    changes log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-
+  before_action :authenticate_user!
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
