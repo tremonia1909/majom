b920546bf6fa13647325b9a9bfd5994ae0f139bf
tremonia1909 <jonathan.peters@freenet.de>
2015-02-18 11:01:17 +0100

    Erster Testfealle Entwurf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource_or_scope)
     pages_index_path
-    UserMailer.welcome_email(current_user)
+    #UserMailer.welcome_email(current_user)
   end
   def after_sign_up_path_for(resource_or_scope)
     root_path
