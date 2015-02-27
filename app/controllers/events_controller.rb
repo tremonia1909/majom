2f72f87f81e2280683ef6b6777df6f87d86121f5
Malte <malte@test.de>
2015-02-27 13:25:02 +0100

    calender sql

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -50,33 +50,41 @@ FROM
   def calendar
     @events = Event.find_by_sql(
         ['
-            select title,description,start_time,end_time
-            FROM
-              (
-                SELECT
-                  project_name AS title,
-                  description  AS description,
-                  start_date   AS start_time,
-                  deadline     AS end_time
-                FROM projects
-              UNION
-
-                SELECT
-                  p1.packet_name  AS title,
-                  p2.project_name AS description,
-                  p1.start_date   AS start_time,
-                  p1.deadline     AS end_time
-                FROM packets as p1
-                join projects as p2
-                  on p1.projects_id = p2.id
-              UNION
-                SELECT
-                  title,
-                  description,
-                  start_time,
-                  end_time
-                FROM events
-              );', current_user.id])
+                select id,title,description,start_time,end_time
+FROM
+  (
+    SELECT
+      id,
+      project_name AS title,
+      description  AS description,
+      start_date   AS start_time,
+      deadline     AS end_time
+    FROM projects p5
+      join user_projects as p6
+        on p5.id=p6.projects_id
+    where p6.users_id=?
+  UNION
+    SELECT
+      p1.id,
+      p1.packet_name  AS title,
+      p2.project_name AS description,
+      p1.start_date   AS start_time,
+      p1.deadline     AS end_time
+    FROM packets AS p1
+      JOIN projects AS p2
+        ON p1.projects_id = p2.id
+      join user_projects as p3
+        on p2.id=p3.projects_id
+       where p3.users_id=?
+  UNION
+    SELECT
+      id,
+      title,
+      description,
+      start_time,
+      end_time
+    FROM events
+  )as g', current_user.id,current_user.id])
   end
 
   def new
